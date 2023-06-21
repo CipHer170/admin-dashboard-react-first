ea92e11456a4fd00ae8c5abf8346978944ff5ed1
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-06-21 16:15:25 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { DataGrid } from "@mui/x-data-grid";
 function App() {
+  const createData = (id, title, description, price) => {
+    return {
+      id,
+      title,
+      description,
+      price,
+    };
+  };
+  const rows = [
+    createData("1", "Lenovo", "Russian company", "$" + 150),
+    createData("2", "Lenovo1", "Russian company1", "$" + 151),
+    createData("3", "Lenovo2", "Russian company2", "$" + 152),
+    createData("4", "Lenovo3", "Russian company3", "$" + 153),
+    createData("5", "Lenovo4", "Russian company4", "$" + 154),
+    createData("6", "Lenovo5", "Russian company5", "$" + 155),
+  ];
+  const columns = [
+    {
+      field: "id",
+      numeric: false,
+      disablePadding: true,
+      width: 50,
+    },
+    {
+      field: "title",
+      numeric: false,
+      disablePadding: true,
+      width: 90,
+    },
+    {
+      field: "description",
+      numeric: false,
+      disablePadding: true,
+      width: 150,
+    },
+    {
+      field: "price",
+      numeric: false,
+      disablePadding: true,
+      width: 100,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        checkboxSelection
+        disableSelectionOnClick
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+      />
     </div>
   );
 }
 
 export default App;
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { StyledEngineProvider } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ***col*****
+// {
+//   id: 'name',
+//   numeric: false,
+//   disablePadding: true,
+//   label: 'Dessert (100g serving)',
+// },
+
+// *****row***
+// {
+//  name: "sjkdhljskd",
+// calories: 23123,
+// fat:1212,
+// carbs:56756,
+// protein:78978,
+// }
