40c2ea72e88b842fbefc109abf98426e1ede1489
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-07-18 16:34:54 +0500

    some styling auth

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { DataContext } from "../../context/DataContextPage";
 import { useNavigate } from "react-router-dom";
-
+import "./login.scss";
+import bkg__img from "../../assets/img/bkg_img.svg";
 function LoginForm() {
   const { setUserToken } = useContext(DataContext);
 
@@ -9,6 +10,7 @@ function LoginForm() {
   const [login, setLogin] = useState();
   const [password, setPassword] = useState();
   const navigate = useNavigate();
+  const [userChoice, setUserChoice] = useState(false);
 
   // const location = useLocation();
   const [isSubmit, setIsSbmit] = useState(false);
@@ -30,10 +32,22 @@ function LoginForm() {
     setLogin(e.target.value);
   };
 
-  useEffect(() => {
+  // false
+  const handleUserChoice = (e) => {
+    setUserChoice(e.target.checked);
+    console.log("userchoice", userChoice);
+    // true
+  };
+
+  const getFromLocalStorage = () => {
     if (localStorage.getItem("user")) {
       const localStorageData = JSON.parse(localStorage.getItem("user"));
       handleSubmitLogin(localStorageData);
+    }
+  };
+  useEffect(() => {
+    if (userChoice === false) {
+      getFromLocalStorage();
     }
     // eslint-disable-next-line
   }, []);
@@ -62,35 +76,62 @@ function LoginForm() {
       setUserToken(userData.idToken);
       if (userData.idToken) {
         navigate("/wrapper");
-        localStorage.setItem("user", JSON.stringify(data));
+        if (userChoice) {
+          localStorage.setItem("user", JSON.stringify(data));
+        }
       }
 
       if (userData.error) {
         setIsSbmit(false);
         return userData.error.message;
       }
-      console.log(userData.registered);
+      // console.log(userData.registered);
     } catch (err) {}
     setIsSbmit(false);
   };
   return (
-    <div>
-      <form onSubmit={submit}>
-        <label htmlFor="">Login</label>
-        <input
-          type="text"
-          placeholder="enter your login"
-          onChange={handleChangeLogin}
-        />
-        <label htmlFor="">Password</label>
-        <input
-          type="text"
-          placeholder="enter your password"
-          onChange={handleChangePassword}
-        />
-        <button disabled={isSubmit}>Log In</button>
-      </form>
-      {errorMessage}
+    <div className="login">
+      {/* {errorMessage} */}
+      <div className="login__form">
+        <h2>Welcome BAck!</h2>
+        <h4>Welcome back! Please enter your details.</h4>
+        <form onSubmit={submit} className="form">
+          <div className="login__input ">
+            <label htmlFor="">Email</label>
+            <input
+              type="text"
+              placeholder="enter your login"
+              onChange={handleChangeLogin}
+            />
+          </div>
+          <div className="login__input ">
+            <label htmlFor="">Password</label>
+            <input
+              type="text"
+              placeholder="enter your password"
+              onChange={handleChangePassword}
+            />
+          </div>
+          <div className="login__checkbox">
+            <div className="login__checkbox-remember">
+              <input
+                type="checkbox"
+                id="remember"
+                onChange={handleUserChoice}
+              />
+              <label htmlFor="remember">Remember me</label>
+            </div>
+            <a href="/">Forgot password</a>
+          </div>
+          <div className="login__action">
+            <button disabled={isSubmit}>Log In</button>
+            <button>Sign In</button>
+          </div>
+        </form>
+      </div>
+      <div className="login__main">
+        <img src={bkg__img} alt="" className="login__img" />
+      </div>
     </div>
   );
 }
--- src/components/Auth/RequiredAuth.js
+++ src/components/Auth/RequiredAuth.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { DataContext } from "../../context/DataContextPage";
-
+import "./login.scss";
 function RequiredAuth({ children }) {
   const location = useLocation();
   const { userToken } = useContext(DataContext);
@@ -10,7 +10,7 @@ function RequiredAuth({ children }) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
 
-  return <div>{children}</div>;
+  return <div className="login-require">{children}</div>;
 }
 
 export default RequiredAuth;
