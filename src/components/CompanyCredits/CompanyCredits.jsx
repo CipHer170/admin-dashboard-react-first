abc7adcd0c601b37dc0c21e7dd950b3035b3c027
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-08-31 11:38:38 +0500

    almost finished

--- src/components/CompanyCredits/CompanyCredits.jsx
+++ src/components/CompanyCredits/CompanyCredits.jsx
@@ -1,29 +1,123 @@
+import axios from "axios";
 import "./CompanyCredits.scss";
+import { Button } from "@mui/material";
+import { useRef } from "react";
+import { BsCloudUpload } from "react-icons/bs";
 function CompanyCredits({
-  compName,
+  compName = "",
   setCompName,
-  compInfo,
+  compInfo = "",
   setCompInfo,
   showModal,
   setShowModal,
+  company,
+  setCompany,
+  compLogo,
+  setCompLogo,
 }) {
-  const handleSubmitCompanyDetails = () => {
-    console.log("sss");
+  const imgGetter = useRef(null);
+
+  const createCompany = async (item) => {
+    try {
+      const res = await axios.post(
+        "https://farman-shopper-default-rtdb.asia-southeast1.firebasedatabase.app/company.json",
+        item
+      );
+      setCompany(res);
+    } catch (err) {
+      console.log("company create", err);
+    }
   };
 
+  const handleNewCompany = (e) => {
+    e.preventDefault();
+    const newCompany = { compName, compInfo, compLogo };
+    createCompany(newCompany);
+    setCompInfo("");
+    setCompName("");
+    setCompany("");
+    setShowModal(!showModal);
+  };
+
+  const handleCompName = (e) => {
+    e.preventDefault();
+    setCompName(e.target.value);
+    console.log(compName);
+  };
+
+  const handleCompInfo = (e) => {
+    setCompInfo(e.target.value);
+    console.log(compInfo);
+  };
+  const handleUploadClick = (e) => {
+    if (e.target.files.length !== 0) {
+      const reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = function () {
+        setCompLogo(reader.result);
+      };
+      e.target = null;
+    }
+  };
+
+  const handleClickPickImg = () => {
+    imgGetter.current.click();
+  };
+  const styledImg = {
+    paddingLeft: "10px",
+    width: "20%",
+    height: "20%",
+  };
   return (
     <div className="company__modal">
       <div className="company__modal_bg "></div>
       <div className="company__modal_window ">
-        <form
-          action=""
-          onSubmit={handleSubmitCompanyDetails}
-          className="company__modal_form"
-        >
+        <form className="company__modal_form">
           <label htmlFor="">Company Name</label>
-          <input className="company__modal_compName " value={compName} />
-          <label htmlFor="">Company Name</label>
-          <input className="company__modal_compInfo " value={compInfo} />
+          <input
+            className="company__modal_compName "
+            value={compName}
+            onChange={handleCompName}
+          />
+          <label htmlFor="">Company Info</label>
+          <input
+            className="company__modal_compInfo "
+            value={compInfo}
+            onChange={handleCompInfo}
+          />
+          <div height={"100%"} width={"50%"} margin={0} padding={0}>
+            <input
+              ref={imgGetter}
+              className="hidden"
+              type="file"
+              onChange={(e) => handleUploadClick(e)}
+            />
+            <Button onClick={!compLogo ? handleClickPickImg : () => 0}>
+              {compLogo ? (
+                <div width={"50%"} height={"50%"}>
+                  <img src={compLogo} alt={compName} style={styledImg} />
+                  <Button
+                    sx={{
+                      position: "absolute",
+                      right: "0",
+                      borderRadius: "50px",
+                      minWidth: "20%",
+                      minHeight: "20%",
+                    }}
+                    onClick={() => setCompLogo("")}
+                  >
+                    X
+                  </Button>
+                </div>
+              ) : (
+                <div textAlign={"center"}>
+                  <h2>Product image</h2>
+                  <BsCloudUpload />
+                </div>
+              )}
+            </Button>
+          </div>
+          <Button onClick={handleNewCompany}>Save</Button>
         </form>
       </div>
     </div>
--- src/components/CompanyCredits/CompanyCreditsPage.jsx
+++ src/components/CompanyCredits/CompanyCreditsPage.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import "./CompanyCredits.scss";
-import logo from "../../assets/img/bkg_img.svg";
 import { Button } from "@mui/material";
 import CompanyCredits from "./CompanyCredits";
+import axios from "axios";
+import { dataFormatter } from "../../context/DataContextPage";
+import noImage from "../../assets/img/no_image.jpg";
+import { FaRegEdit } from "react-icons/fa";
+import NewItem from "../NewItem/NewItem";
 
 function CompanyCreditsPage() {
-  const [compName, setCompName] = useState("CompName");
-  const [compInfo, setCompInfo] = useState(
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae blanditiis tempore nostrum recusandae expedita eius repellat itaque nulla, voluptatum architecto?"
-  );
+  const [compName, setCompName] = useState();
+  const [compInfo, setCompInfo] = useState();
+  const [compLogo, setCompLogo] = useState();
+  const [company, setCompany] = useState([]);
+
   const [data, setData] = useState(false);
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const transferringData = {
     compName,
     setCompName,
@@ -19,30 +24,76 @@ function CompanyCreditsPage() {
     setCompInfo,
     showModal,
     setShowModal,
+    company,
+    setCompany,
+    compLogo,
+    setCompLogo,
+  };
+  useEffect(() => {
+    getCompany();
+  }, []);
+  const getCompany = async () => {
+    try {
+      const res = await axios.get(
+        "https://farman-shopper-default-rtdb.asia-southeast1.firebasedatabase.app/company.json"
+      );
+      setCompany(dataFormatter(res.data));
+    } catch (error) {
+      console.warn(error);
+    }
   };
 
+  const editName = async (newCompName, id) => {
+    await axios.put(
+      `https://farman-shopper-default-rtdb.asia-southeast1.firebasedatabase.app/company/${id}.json`,
+      newCompName
+    );
+    const newData = { ...newCompName, id };
+    const updateData = compName.map((item) => {
+      if (item.id === id) {
+        return newData;
+      }
+      return item;
+    });
+    setCompName(updateData);
+  };
+
+  const handleEditName = (e) => {
+    e.preventDefault();
+    const newProduct = { compInfo, compName, compLogo };
+    editName(newProduct);
+  };
   return (
     <div>
       {showModal ? (
-        <Button onClick={() => setShowModal(!showModal)}>Add</Button>
+        <CompanyCredits {...transferringData} />
       ) : (
-        <CompanyCredits transferringData={transferringData} />
+        <h1>Company Details</h1>
       )}
 
-      {data ? (
+      {company.length > 0 ? (
         <div className="compName">
           <div className="compName__container">
-            <h2>name</h2>
-            <pre className="compName__name_data">{compName}</pre>
-            <h2>info</h2>
-            <pre className="compName__info_data">{compInfo}</pre>
-            <Button onClick={() => setShowModal(!showModal)}>exit</Button>
-          </div>
+            <h3>Company name</h3>
 
-          <img src={logo} alt="" className="compName__img" />
+            <pre className="compName__name_data hide_btn">
+              {company[0].compName}{" "}
+              <FaRegEdit className="edit_btn" onClick={handleEditName} />
+            </pre>
+            <h3>Company info</h3>
+            <pre className="compName__info_data hide_btn">
+              {company[0].compInfo}{" "}
+              <FaRegEdit className="edit_btn" onClick={handleEditName} />
+            </pre>
+          </div>
+          <img
+            src={company[0].compLogo ? company[0].compLogo : noImage}
+            alt="company__logo"
+            className="compName__logo"
+          />
         </div>
       ) : (
-        ""
+        <Button onClick={() => setShowModal(!showModal)}>Add Company</Button>
       )}
     </div>
   );
